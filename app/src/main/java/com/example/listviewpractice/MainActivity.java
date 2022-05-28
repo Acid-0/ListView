983c981867178e122390e954019e3f390e9d92c2
myhtml <bsef19a015@pucit.edu.pk>
2022-05-28 21:46:11 +0500

    listView with arraylist

--- app/src/main/java/com/example/listviewpractice/MainActivity.java
+++ app/src/main/java/com/example/listviewpractice/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
@@ -14,11 +16,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String [] list = {"ali","ahmad","amna","alex","ali","aahdmad","aldsfi","auuuhmad","amli",
-                "irsa","kash","alsh","zindagi","aloo","akhatar","anam"};
+       /* String [] list = {"ali","ahmad","amna","alex","ali","aahdmad","aldsfi","auuuhmad","amli",
+                "irsa","kash","alsh","zindagi","aloo","akhatar","anam"};*/
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("zubi");
+        list.add("kash");
+        list.add("aaa");
+        list.add("zubi");
+        list.add("kash");
+        list.add("aaa");
+        list.add("zubi");
+        list.add("kash");
+        list.add("aaa");
+        list.add("zubi");
+        list.add("kash");
+        list.add("aaa");
+        list.add("zubi");
+        list.add("kash");
+        list.add("aaa");
         listView = findViewById(R.id.list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
         listView.setAdapter(adapter);
-        
+
     }
 }
